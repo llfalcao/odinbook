7ae68b7572b85aa1e1fb59e0e286723e0b33b035
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-01-09 22:39:19 -0300

    Add initial route testing

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", router);
+
+// TODO: Handle errors
+
+module.exports = app;
