0f7fdf1b10d414c60d4afd678361295d7c4fed1f
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-01-10 21:28:26 -0300

    Use a promise instead

--- src/test/routes.test.js
+++ src/test/routes.test.js
@@ -1,13 +1,9 @@
-const request = require("supertest");
-const app = require("../app");
+import request from "supertest";
+import app from "../app";
 
 describe("Test the root path", () => {
-  it("should response the GET method", (done) => {
-    request(app)
-      .get("/")
-      .then((response) => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
+  it("should response the GET method", async () => {
+    const response = await request(app).get("/");
+    return expect(response.statusCode).toBe(200);
   });
 });
