74beb7e8635b29f9e07408b747bd3746971e0dba
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-01-10 22:19:35 -0300

    Change prettier settings

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const router = require("./routes");
+const express = require('express');
+const router = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", router);
+app.use('/', router);
 
 // TODO: Handle errors
 
--- src/test/routes.test.js
+++ src/test/routes.test.js
@@ -1,9 +1,9 @@
-import request from "supertest";
-import app from "../app";
+import request from 'supertest';
+import app from '../app';
 
-describe("Test the root path", () => {
-  it("should response the GET method", async () => {
-    const response = await request(app).get("/");
+describe('Test the root path', () => {
+  it('should response the GET method', async () => {
+    const response = await request(app).get('/');
     return expect(response.statusCode).toBe(200);
   });
 });
