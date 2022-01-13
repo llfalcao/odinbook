64bc08ed74fa4f2ed2cdbbcaa5a0a688c8698fe3
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-01-12 22:04:32 -0300

    Create 'Comment' model

---
+++ src/models/Comment.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  post_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  depth: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  parent_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'Comment',
+    default: null,
+  },
+  children: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
+});
+
+CommentSchema.virtual('url').get(function () {
+  return `/posts/${post_id}/${_id}`;
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
