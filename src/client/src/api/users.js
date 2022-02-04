d89379e85f2b772b90f75fe3f0db0fd19845958a
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-02-03 21:56:41 -0300

    Create Friendlist component

--- src/client/src/api/users.js
+++ src/client/src/api/users.js
@@ -7,3 +7,11 @@ export async function fetchUserById(userId) {
   });
   return await response.json();
 }
+
+export async function fetchFriends(username) {
+  const api = userApis.read;
+  const response = await fetch(`${api.url}/${username}/friends`, {
+    method: api.method,
+  });
+  return await response.json();
+}
---
+++ src/client/src/components/Friendlist.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { fetchFriends } from '../api/users';
+
+export default function Friendlist({ username }) {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    fetchFriends(username).then((list) => setFriends(list));
+  }, [username]);
+
+  return (
+    <div className="friendlist friendlist--sidebar">
+      <h4>Friends</h4>
+      <ul>
+        {friends.map((user) => (
+          <li key={user._id}>
+            <div className="profilePicture--small">
+              <img src={user.profile_pic} alt="" />
+            </div>
+            <p>{user.full_name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
